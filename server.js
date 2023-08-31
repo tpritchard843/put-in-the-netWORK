5be807889d681a44d3a575c6041392dd9a46c7fb
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-08-31 13:30:30 -0400

    Changes

--- server.js
+++ server.js
@@ -1,114 +1,133 @@
-const morgan = require('morgan')
-const express = require('express')
-const app = express()
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const PORT = 3001;
 
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
+app.use(express.json());
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'));
+morgan.token('object', (req, res) =>  `${JSON.stringify(req.body)}`);
 
-morgan.token('object', (req, res) =>  `${JSON.stringify(req.body)}`)
+// Dummy data for rolodex = [{},{},{},{}]
 
-
-let phonebook = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  },
-  {
-    "id": 5,
-    "name": "Johnny Bravo",
-    "number": "86-75-3093457"
-  }
-]
+// let rolodex = [
+//   {
+//     "id": 1,
+//     "name": "Arto Hellas",
+//     "email": "040-123456",
+//     "company": "Google",
+//     "dateAdded":"2023-03-22",
+//     "spark":"Likes Pokemon",
+//   },
+//   {
+//     "id": 2,
+//     "name": "Ada Lovelace",
+//     "email": "39-44-5323523",
+//     "company": "Apple",
+//     "dateAdded":"2022-11-07",
+//     "spark":"Bookworm",
+//   },
+//   {
+//     "id": 3,
+//     "name": "Dan Abramov",
+//     "email": "12-43-234345",
+//     "company":"Netflix",
+//     "dateAdded":"2023-05-14",
+//     "spark":"Cubs fan",
+//   },
+//   {
+//     "id": 4,
+//     "name": "Mary Poppendieck",
+//     "email": "39-23-6423122",
+//     "company": "Meta",
+//     "dateAdded":"08-28-2023",
+//     "spark":"Pizza lover",
+//   },
+//   {
+//     "id": 5,
+//     "name": "Johnny Bravo",
+//     "email": "86-75-3093457",
+//     "company": "Amazon",
+//     "dateAdded":"02-14-2023",
+//     "spark":"Bruce Springsteen",
+//   }
+// ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  // send index.html
+  res.send('<h1>Hello World!</h1>');
 })
 
 app.get('/info', (req,res) => {
   res.send(`<p>Phonebook has info for ${phonebook.length} people</p>
-  <p>${Date()}</p>`)
+  <p>${Date()}</p>`);
 })
 
 const generateId = () => {
-  const maxId = phonebook.length > 0
-    ? Math.max(...phonebook.map(n => n.id))
-    : 0
-  return maxId + 1
+  const maxId = rolodex.length > 0
+    ? Math.max(...rolodex.map(n => n.id))
+    : 0;
+  return maxId + 1;
 }
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
-  console.log(body.name)
-  console.log(phonebook)
+  console.log('Helloooooooooooooo')
+  // const body = request.body;
+  // console.log(body.name);
+  // console.log(rolodex);
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name is missing'
-    })
-  }
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number is missing'
-    })
-  }
+  // if (!body.name) {
+  //   return response.status(400).json({
+  //     error: 'name is missing'
+  //   })
+  // }
+  // if (!body.number) {
+  //   return response.status(400).json({
+  //     error: 'number is missing'
+  //   })
+  // }
 
-  if (phonebook.some(person => person.name === body.name)) {
-    return response.status(409).json({
-      error: 'name must be unique'
-    })
-  }
+  // if (rolodex.some(person => person.name === body.name)) {
+  //   return response.status(409).json({
+  //     error: 'name must be unique'
+  //   })
+  // }
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
+  // const person = {
+  //   id: generateId(),
+  //   name: body.name,
+  //   email: body.email,
+  //   company: body.company,
+  //   dateAdded: body.dateAdded
+  // }
 
-  phonebook = phonebook.push(person)
+  // rolodex = rolodex.push(person);
 
-  response.json(person)
+  // response.json(person);
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
+  res.json(rolodex);
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  phonebook = phonebook.filter(person => person.id !== id)
+  const id = Number(request.params.id);
+  rolodex = rolodex.filter(person => person.id !== id);
 
-  response.status(204).end()
+  response.status(204).end();
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = phonebook.find(person => person.id === id)
+  const id = Number(request.params.id);
+  const person = rolodex.find(person => person.id === id);
 
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
 
   }
 })
 
-const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 })
